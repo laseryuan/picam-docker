23d00f7d75c4b2b5028316c8644379e708bef613
Laser Yuan <laser.yuan@gmail.com>
2018-05-03 14:49:10 +0800

    Record video file in 10 minutes pieces

---
+++ picam/record.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Replace ~/picam with your picam directory
+cd /root/picam || exit 1
+
+# Start picam
+./picam --time --width 640 --height 480 --videobitrate 500000 --fps 20 --noaudio --tcpout tcp://rtmp:8181 &
+sleep 30
+
+# Stop recording on Ctrl-C
+trap on_sigint INT
+on_sigint() {
+  echo "Stop recording"
+  touch hooks/stop_record
+  exit 0
+}
+
+# Do forever
+while :
+do
+  echo "Start recording"
+  touch hooks/start_record || exit 1
+
+  echo "Waiting for one hour..."
+  sleep 10m
+
+  echo "Stop recording"
+  touch hooks/stop_record || exit 1
+
+  # Wait for recording to stop
+  while [ `cat state/record` = "true" ]; do
+    sleep 0.1
+  done
+done

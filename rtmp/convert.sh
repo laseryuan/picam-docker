f65175244c110a2ead1742d3816b9e38c6b16c3d
Laser Yuan <laser.yuan@gmail.com>
2018-05-03 22:29:36 +0800

    Add check_disk_usage()

--- rtmp/convert.sh
+++ rtmp/convert.sh
@@ -12,11 +12,32 @@ on_sigint() {
   exit 0
 }
 
+check_disk_usage() {
+  while :
+  do
+    echo "Check disk capacity..."
+    used_percentage=$(df --output=pcent / | tr -dc '0-9')
+    if [ $used_percentage -gt 95 ]; then
+      echo "Disk usage: $used_percentage%, trying to trim files ..."
+      latestfile=$(ls -1rt *.mp4 | head -n1)
+      rm "$latestfile"
+    else
+      break
+    fi
+
+    echo "Waiting for 10 seconds..."
+    sleep 10
+  done
+}
+
 # Do forever
 while :
 do
+  check_disk_usage
+
   ts_files_number=$(ls -1t ./*.ts | wc -l)
   echo "$ts_files_number ts files left."
+
   if [ $ts_files_number -gt 1 ]; then
     echo "Start converting..."
     latestfile=$(ls -1t ./*.ts | head -n2 | tail -n1 | sed -e 's/\.ts$//')

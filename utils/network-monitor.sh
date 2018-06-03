b458a96a1efb39f8b879b40fc61f4b7cf13a6c23
Laser Yuan <laser.yuan@gmail.com>
2018-06-03 11:42:55 +0000

    Add network-monitor.sh

---
+++ utils/network-monitor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##################################################################
+# Settings
+# Which Interface do you want to check/fix
+wlan='wlan0'
+# Which address do you want to ping to see if the network interface is alive?
+pingip='192.168.21.1'
+##################################################################
+
+echo "Performing Network check for $wlan"
+/bin/ping -c 1 -I $wlan $pingip > /dev/null 2> /dev/null
+if [ $? -ge 1 ] ; then
+    echo "Network: $wlan connection down! Attempting reconnection."
+    ifup --force wlan0
+else
+    echo "Network is Okay"
+fi

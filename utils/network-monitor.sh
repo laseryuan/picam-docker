eb32b4bd1907eb159cfbffd5c57b0b7617fd9f5b
Laser Yuan <laser.yuan@gmail.com>
2018-08-01 23:10:34 +0800

    Fix: ifup not found

--- utils/network-monitor.sh
+++ utils/network-monitor.sh
@@ -12,7 +12,7 @@ echo "Performing Network check for $wlan"
 /bin/ping -c 1 -I $wlan $pingip > /dev/null 2> /dev/null
 if [ $? -ge 1 ] ; then
     echo "Network: $wlan connection down! Attempting reconnection."
-    ifup --force wlan0
+    /sbin/ifup --force wlan0
 else
     echo "Network is Okay"
 fi

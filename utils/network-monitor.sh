5bda24338a9f583002da5851a6fa98917c718a67
Laser Yuan <laser.yuan@gmail.com>
2018-08-02 05:12:58 +0000

    Log when finish reconnecting

--- utils/network-monitor.sh
+++ utils/network-monitor.sh
@@ -11,12 +11,13 @@ pingip='114.114.114.114'
 echo "Performing Network check for $wlan"
 /bin/ping -c 1 -I $wlan $pingip > /dev/null 2> /dev/null
 if [ $? -ge 1 ] ; then
-    echo "Try aggain"
+    echo "Looks like down! Try aggain..."
     /bin/ping -c 1 -I $wlan $pingip
     if [ $? -ge 1 ] ; then
-        echo "Network: $wlan connection down! Attempting reconnection."
-        ip link set wlan0 down
-        ip link set wlan0 up
+        echo "Network: $wlan connection down! Attempting to reconnect..."
+        ip link set $wlan down
+        ip link set $wlan up
+        echo "... Done"
     fi
 else
     echo "Network is Okay"

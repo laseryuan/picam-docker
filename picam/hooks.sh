78d99552c10dc2860d8d6bd5b1631163387865fa
Laser Yuan <laser.yuan@gmail.com>
2020-07-06 13:48:00 +0800

    Add restart_picam()

--- picam/hooks.sh
+++ picam/hooks.sh
@@ -18,13 +18,30 @@ picam_is_alive() {
   pgrep -x picam
 }
 
+restart_picam() {
+  echo "Picam is dead. Restarting picam ..."
+  echo "picam $(cat ~/picam_param) &"
+  picam $(cat ~/picam_param) &
+  sleep 5
+}
+
 health_check() {
   while ! picam_is_alive; do
-    echo "Picam is dead. Restarting picam ..."
-    echo "picam $(cat ~/picam_param) &"
-    picam $(cat ~/picam_param) &
-    sleep 5
+    echo "Picam is dead."
+    restart_picam
   done
+}
+
+start_record() {
+  echo "Start recording"
+  touch hooks/start_record || exit 1
+  sleep 5
+  if [ `cat state/record` = "false" ]; then
+    echo "Record stop working. Kill picam and restart ..."
+    kill -9 $(pidof picam)
+    restart_picam
+    start_record
+  fi
 }
 
 # Set global recordbuf to 30
@@ -35,8 +52,7 @@ while :
 do
   health_check
 
-  echo "Start recording"
-  touch hooks/start_record || exit 1
+  start_record
 
   echo "Keep recording for about ten minutes..."
   wait_for_about_ten_minutes
